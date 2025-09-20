6c611341779498128088b33870c1d26ffe42ed7a
lolaalabi <omololaalabi.a@gmail.com>
2025-09-20 23:51:03 +0100

    Initial commit

---
+++ practice.js
@@ -0,0 +1,95 @@
+// console.dir(document);
+// console.log(document.domain);
+// console.log(document.url);
+// console.log(document.title);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.doctype);
+// console.log(document.all[10]);
+// const whatever = document.getElementById("title");
+// console.log(whatever.innerText);
+
+// const sub = document.getElementsByClassName("sub");
+// console.log(sub[0].innerText);
+
+// const button = document.getElementsByTagName("button");
+// console.log(button[0].innerText);
+
+// const sub = document.querySelector(".sub");
+// console.log(title.innerText);
+
+// const heading = document.querySelector("#heading");
+// heading.innerHTML = "This is a new heading";
+
+// document.querySelector("button").setAttribute("disabled", true);
+// document.querySelector("h1").style.color = "blue";
+
+// document.querySelector("p").style.color = "red";
+// document.querySelector("p").style.fontSize = "Small";
+// document.querySelector("P").innerText = "This is another paragraph"
+
+// const heading = document.querySelector("h1");
+
+// btn.addEventListener("click", function () {
+//     console.log(input.value);
+// //  heading.innerText = "this is the one created by addEventListener";
+// });
+
+// const input = document.querySelector("input");
+// const btn = document.querySelector("button");
+
+// btn.addEventListener("click", function () {
+//   alert (input.value);
+// });
+
+// const username = document.querySelector("#username");
+// const password = document.querySelector("#password");
+
+// const btn = document.querySelector("button");
+// const paragraph = document.querySelector("p");
+
+// btn.addEventListener("click", function () {
+//  paragraph.style.color = "blue";
+// })
+
+// const changeText = () => {
+//   paragraph.innerText = "This is a whole new paragraph";
+// };
+
+// const newElement = document.createElement("P");
+// newElement.innerText = "This is a new paragraph";
+// document.body.appendChild(newElement);
+
+// const btn = document.querySelector("button");
+// const remove_btn = document.querySelector("#remove-btn")
+
+
+
+
+// btn.addEventListener("click", function () {
+//   const newElement = document.createElement("div");
+//   newElement.innerText = "product";
+//   document.body.appendChild(newElement);
+// })
+
+// remove_btn.addEventListener("click", function () {
+//   btn.remove();
+// });
+
+// const newElement = document.createElement("li");
+// newElement.innerText = "List Item";
+// document.body.appendChild(newElement);
+//createElement
+
+const newDiv = document.createElement("div");
+newDiv.innerText = "Hello";
+document.body.appendChild(newDiv);
+
+// newDiv.className = "Hello";
+// newDiv.id = "Hello1";
+
+// var newDivText = document.createTextNode("Hello World");
+// newDiv.appendChild(newDivText);
+
+// console.log(newDiv);
+ 
